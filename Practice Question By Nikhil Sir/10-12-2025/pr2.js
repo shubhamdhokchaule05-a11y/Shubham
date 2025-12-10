731bbd019bf023b4613460a4b8b7c084a4a05873
shubhamdhokchaule05-a11y <shubhamdhokchaule05@gmail.com>
2025-12-10 18:25:17 +0530

    add this to project

--- Practice Question By Nikhil Sir/10-12-2025/pr2.js
+++ Practice Question By Nikhil Sir/10-12-2025/pr2.js
@@ -1,4 +1,4 @@
 // 2) Write a JavaScript program using a for loop to print the multiplication table of 7 (from 7×1 to 7×10).
 for (let i = 1; i <= 10; i++) {
-    console.log(`7 * ${i} = ${7 * i}`);
+    console.log(`7 x ${i} = ${7 * i}`);
 }
